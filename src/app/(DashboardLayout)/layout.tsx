0e1bc0d43b7c3fcb63f30b7fbf6db62ccc26ff62
Muhammad-Zuhaib13 <muhammadzohaib578@gmail.com>
2026-02-10 10:57:22 +0500

    add

--- src/app/(DashboardLayout)/layout.tsx
+++ src/app/(DashboardLayout)/layout.tsx
@@ -1,6 +1,12 @@
-import DashboardLayout from "./DashboardLayout";
+"use client";
 
+import dynamic from "next/dynamic";
+import { ReactNode } from "react";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const DashboardLayout = dynamic(() => import("./DashboardLayout"), {
+  ssr: false,
+});
+
+export default function Layout({ children }: { children: ReactNode }) {
   return <DashboardLayout>{children}</DashboardLayout>;
 }
